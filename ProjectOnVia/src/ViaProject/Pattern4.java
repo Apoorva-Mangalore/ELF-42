f03e6111b8229d721fb39cc35d58ea48af3b0aba
GOPI <GOPI@GOPI-PC>
2023-01-23 14:46:25 +0530

    New script

---
+++ ProjectOnVia/src/ViaProject/Pattern4.java
@@ -0,0 +1,18 @@
+package ViaProject;
+
+public class Pattern4 {
+public static void main(String[] args) {
+	for(int i=0;i<=4;i++) {
+		for(int j=0;j<=3;j++) {
+			if(j==2) {
+				System.out.print("*");
+			}
+			else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+	
+}
+}
